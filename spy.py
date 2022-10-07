25e20c88e942a46fdd75fce764fd2ab1591edc5a
Maria <marykor1997@gmail.com>
2022-10-07 22:21:49 +0300

    log1.log

--- spy.py
+++ spy.py
@@ -7,3 +7,10 @@ def log(msg):
                f'Сообщение пользователя: {msg.text}\n')
     file.close()
 
+
+def log1(msg, answer):
+    file = open('log1.log', 'a', encoding='utf-8')
+    file.write(f'Дата: {datetime.datetime.now().date()}\nВремя: {datetime.datetime.now().time()}\nID пользователя: {msg.from_user.id}\n'
+               f'Сообщение пользователя: {msg.text}\nСообщение от бота: {answer}\n')
+    file.close()
+
